eafe8319b6979fc1c433b35f029e17721a40e3cd
Jason Tsukahara <tsuky19@gmail.com>
2018-11-09 09:00:40 -0500

    add type parameter

--- R/bin.score.R
+++ R/bin.score.R
@@ -2,45 +2,50 @@
 #'
 #' Calculates Bin scores using the method suggested in [insert citation here]
 #' @param x a dataframe with trial level data. Needs to have RT and Accuracy DVs
-#' @param RT.label Column name in dataframe that contains the reaction time data
-#' @param Accuracy.label Column name in dataframe that contains the accuracy data
-#' @param Subject.label Column name in dataframe that contains the subject identifiers
-#' @param Condition.label Column name in dataframe that contains the trial condition type
+#' @param rt.col Column name in dataframe that contains the reaction time data
+#' @param accuracy.col Column name in dataframe that contains the accuracy data
+#' @param condition.col Column name in dataframe that contains the trial condition type
+#' @param type How should Bin trials be aggregated, "sum" or "mean" (Default: "mean")
 #' @param baseline.condition The values that specify the baseline condition
+#' @param id Column name in dataframe that contains the subject identifiers
 #' @keywords bin
 #' @export
 #' @examples
 #' bin.score(data, RT.label = "RT", Accuracy.label = "Accuracy", Condition.label = "TrialType", Subject.label = "Subject")
 
-bin.score <- function(x,
-                      RT.label = "RT", Accuracy.label = "Accuracy", Condition.label = "Condition", Subject.label = "Subject",
-                      baseline.condition = "congruent"){
-  labels <- c(Subject.label, RT.label, Accuracy.label, Condition.label)
-  for (label in labels){
-    if (label==Subject.label){
-      change <- "Subject"
-    } else if (label==RT.label){
-      change <- "RT"
-    } else if (label==Accuracy.label){
-      change <- "Accuracy"
-    } else if (label==Condition.label){
-      change <- "TrialType"
-    }
-    colnames(x)[which(colnames(x)==label)] <- change
-  }
-  x <- dplyr::group_by(x, Subject, TrialType)
+bin.score <- function(x, rt.col = "RT", accuracy.col = "Accuracy", condition.col = "Condition",
+                      baseline.condition = "congruent", type = "mean", id = "Subject"){
+
+  colnames(x)[which(colnames(x)==id)] <- "Subject"
+  colnames(x)[which(colnames(x)==rt.col)] <- "RT"
+  colnames(x)[which(colnames(x)==accuracy.col)] <- "Accuracy"
+  colnames(x)[which(colnames(x)==condition.col)] <- "Condition"
+
+  x <- dplyr::group_by(x, Subject, Condition)
   x <- dplyr::mutate(x, MeanRT = stats::ave(RT),
-              baselineMeanRT = ifelse(TrialType==baseline.condition, MeanRT, NA))
+              baselineMeanRT = ifelse(Condition==baseline.condition, MeanRT, NA))
   x <- dplyr::group_by(x, Subject)
   x <- dplyr::mutate(x, baselineMeanRT = stats::ave(baselineMeanRT, FUN = function(y) mean(y, na.rm = TRUE)))
-  x <- dplyr::filter(x, TrialType!=baseline.condition)
+  x <- dplyr::filter(x, Condition!=baseline.condition)
   x <- dplyr::ungroup(x)
   x <- dplyr::group_by(x, Accuracy)
   x <- dplyr::mutate(x, RTDif = RT - baselineMeanRT,
               Bin = ifelse(Accuracy==1, dplyr::ntile(RTDif, 10), ifelse(Accuracy==0, 0, NA)),
               Bin = ifelse(Bin==0, 20, Bin))
   x <- dplyr::group_by(x, Subject)
-  x <- dplyr::summarise(x, BinScore = mean(Bin, na.rm = TRUE))
-  colnames(x)[which(colnames(x)=="Subject")] <- Subject.label
+
+  if (type == "mean"){
+    x <- dplyr::summarise(x, BinScore = mean(Bin, na.rm = TRUE))
+  }
+
+  if (type = "sum"){
+    x <- dplyr::summarise(x, BinScore = sum(Bin, na.rm = TRUE))
+  }
+  x <- dplyr::ungroup(x)
+
+  colnames(x)[which(colnames(x)=="Subject")] <- id
+  colnames(x)[which(colnames(x)=="RT")] <- rt.col
+  colnames(x)[which(colnames(x)=="Accuracy")] <- accuracy.col
+  colnames(x)[which(colnames(x)=="Condition")] <- condition.col
   return(x)
 }
