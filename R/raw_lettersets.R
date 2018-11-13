ff28de370406b70b80bfe9a851fdcd427bf43d5b
Jason Tsukahara <tsuky19@gmail.com>
2018-11-13 18:55:46 -0500

    deprecate raw.lettersets

--- R/raw_lettersets.R
+++ R/raw_lettersets.R
@@ -2,22 +2,12 @@
 #'
 #' Creates a raw data file of the LetterSets task from the E-Merge file
 #' @param x a .txt exported E-Merge (or single E-Data) file
-#'
-#' @templateVar fun raw.lettersets
-#' @template template-depr_fun
-NULL
-
-#' @templateVar old raw.lettersets
-#' @templateVar new raw_lettersets
-#' @template template-depr_pkg
-#'
 #' @keywords raw
 #' @export
 #' @examples
-#' raw.lettersets(data)
+#' raw_lettersets(data)
 
-raw.lettersets <- function(x){
-  .Deprecated("raw_lettersets")
+raw_lettersets <- function(x){
   x <- dplyr::filter(x, Blocks=="Real"|Blocks=="End", ShowStim.RT>0|!is.na(TotalScore))
   x <- dplyr::select(x, Subject, Trial, Answer = answer, Response = ItemResp,
                      Accuracy = ShowStim.ACC, RT = ShowStim.RT, TimeLeft = StopTime, TotalScore,
