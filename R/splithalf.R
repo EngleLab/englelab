53116479b94afc8b446228cf0c1b5fa01d031b64
dr-JT <tsuky19@gmail.com>
2019-02-13 18:39:01 -0500

    add aggregate param

--- R/splithalf.R
+++ R/splithalf.R
@@ -4,13 +4,14 @@
 #' @param x a dataframe with trial level data
 #' @param trial.col The column name that identifies trials
 #' @param value The column name that identifies the values to be used
+#' @param aggregate How should the value column be aggregated across even and odd trials? (ex. mean or sum)
 #' @param id The column name that identifies the Subject IDs.
 #' @keywords cronbach
 #' @export
 #' @examples
 #'
 
-splithalf <- function(x, trial.col = "Trial", value = "", id = "Subject"){
+splithalf <- function(x, trial.col = "Trial", value = "", aggregate = NULL, id = "Subject"){
   if (value != ""){
     colnames(x)[which(colnames(x)==trial.col)] <- "Trial"
     trials <- max(x$Trial, na.rm = TRUE)
@@ -27,8 +28,14 @@ splithalf <- function(x, trial.col = "Trial", value = "", id = "Subject"){
   }
   even.col <- colnames(x)[c(FALSE,TRUE)]
   odd.col <- colnames(x)[c(TRUE,FALSE)]
-  x <- transform(x, even = rowMeans(x[even.col], na.rm = TRUE))
-  x <- transform(x, odd = rowMeans(x[odd.col], na.rm = TRUE))
+  if (aggregate=="mean"){
+    x <- transform(x, even = rowMeans(x[even.col], na.rm = TRUE))
+    x <- transform(x, odd = rowMeans(x[odd.col], na.rm = TRUE))
+  }
+  if (aggregate=="sum"){
+    x <- transform(x, even = rowSums(x[even.col], na.rm = TRUE))
+    x <- transform(x, odd = rowSums(x[odd.col], na.rm = TRUE))
+  }
 
   r <- cor(x$even, x$odd, use = "pairwise.complete.obs")
   sh <- (2*r)/(1+r)
