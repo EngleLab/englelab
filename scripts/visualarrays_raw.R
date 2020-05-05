ae157ee25c4862dd73f81959d902f1adc2739fa3
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2020-05-05 10:11:34 -0400

    Update visualarrays_raw.R

--- scripts/visualarrays_raw.R
+++ scripts/visualarrays_raw.R
@@ -1,32 +1,54 @@
-## Set up ####
+#### Setup ####
 ## Load packages
-library(readr)
 library(here)
-library(englelab)
+library(readr)
+library(dplyr)
 
-## Set import/output directories
+## Set Import/Output Directories
 import_dir <- "Data Files/Merged"
 output_dir <- "Data Files"
 
-## Set import/output files
-task <- "VAorient_S"
-import.file <- paste(task, ".txt", sep = "")
-output.file <- paste(task, "_raw.csv", sep = "")
-##############
-
+## Set Import/Output Filenames
+task <- "taskname"
 import_file <- paste(task, ".txt", sep = "")
-output_file <- paste(task, "_raw.csv", sep = "")
-##############
+output_file <- paste(task, "raw.csv", sep = "_")
+################
 
-## Import Data
-data_import <- read_delim(here(import_dir, import_file), "\t",
-                          escape_double = FALSE, trim_ws = TRUE,
-                          guess_max = 10000)
+#### Import ####
+data_import <- read_delim(here(import_dir, import_file),
+                     "\t", escape_double = FALSE, trim_ws = TRUE)
+################
 
-## Clean up raw data
-data_raw <- raw_visualarrays(data_import)
+#### Tidy raw data ####
+data_raw <- data_import %>%
+  rename(TrialProc = `Procedure[Trial]`) %>%
+  filter(TrialProc == "showproc" | TrialProc == "pracproc") %>%
+  mutate(TrialProc = case_when(TrialProc == "showproc" ~ "real",
+                               TrialProc == "pracproc" ~ "practice"),
+         Response = case_when(VisResponse.RESP == 5 ~ "same",
+                              VisResponse.RESP == 6 ~ "different",
+                              TRUE ~ as.character(NA)),
+         CorrectResponse = case_when(VisResponse.CRESP == 5 ~ "same",
+                                     VisResponse.CRESP == 6 ~ "different"),
+         CorrectRejection = case_when(CorrectResponse == "same" & 
+                                        Response == "same" ~ 1,
+                                      TRUE ~ 0),
+         FalseAlarm = case_when(CorrectResponse == "same" & 
+                                  Response == "different" ~ 1,
+                                TRUE ~ 0),
+         Miss = case_when(CorrectResponse == "different" & 
+                            Response == "same" ~ 1,
+                          TRUE ~ 0),
+         Hit = case_when(CorrectResponse == "different" & 
+                           Response == "different" ~ 1,
+                         TRUE ~ 0)) %>%
+  select(Subject, TrialProc, Trial, SetSize, Accuracy = VisResponse.ACC, 
+         Response, CorrectResponse, CorrectRejection, FalseAlarm, Miss, Hit, 
+         SessionDate, SessionTime)
+#######################
 
-## Save Data
-write_csv(data_raw, path = here(output_dir, output_file))
+#### Output ####
+write_csv(data_raw, here(output_dir, output_file))
+################
 
 rm(list=ls())
