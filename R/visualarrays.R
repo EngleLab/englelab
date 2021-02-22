edacbd5ff5537d9de13ae3d2d67d3169f6ac7784
Jason Tsukahara <jtsukahara3@gatech.edu>
2021-02-22 17:53:25 -0500

    update to v1.0

--- R/visualarrays.R
+++ R/visualarrays.R
@@ -81,6 +81,7 @@ raw_visualarrays <- function(x, taskVersion = "new"){
 #'
 
 score_visualarrays <- function(x){
+  x <- dplyr::group_by(x, .add = TRUE)
   x <- dplyr::summarise(x,
                         CR.n = sum(CorrectRejection, na.rm = TRUE),
                         FA.n = sum(FalseAlarm, na.rm = TRUE),
