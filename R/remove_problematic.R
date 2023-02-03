82074e00ad41f8ab4a68d5dc0ba4b2fa9416ab9b
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-02-03 09:43:45 -0500

    change filter expression to remove

--- R/remove_problematic.R
+++ R/remove_problematic.R
@@ -1,28 +1,28 @@
 #' Remove Problematic Subjects
 #'
-#' This function will remove problematic subjects based on a filter criteria
+#' This function will remove problematic subjects based on a filter criterion
 #' and save them to a log data file.
 #'
 #' @param x dataframe
-#' @param filter an expression passed on to dplyr:filter()
+#' @param remove an expression passed on to dplyr:filter()
 #' @param log_file the file path and name for where to save a log file to
 #' @export
 #'
 
 remove_problematic <- function(x,
-                             filter = "",
-                             log_file = NULL) {
+                               remove = "",
+                               log_file = NULL) {
 
   filter_func <-
     rlang::parse_expr(
-    paste("dplyr::filter(x, ", filter, ")", sep = "")
+    paste("dplyr::filter(x, ", remove, ")", sep = "")
     )
   x_removed <- eval(filter_func)
+  x_keep <- dplyr::anti_join(x, x_removed)
 
-  if (!is.null(log_file)) {
-    removed <- dplyr::anti_join(x, x_removed)
-    readr::write_csv(removed, log_file)
+  if (!is.null(log_file) & nrow(x_removed > 0)) {
+    readr::write_csv(x_removed, log_file)
   }
 
-  return(x_removed)
+  return(x_keep)
 }
