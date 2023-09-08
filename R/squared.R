78b9ca5b899c2ccaa387c0dab2ea8e099c01e756
Tsukahara <jtsukahara3@ipsec-10-2-97-30.vpn.gatech.edu>
2023-09-08 17:39:31 -0400

    update logo

---
+++ R/squared.R
@@ -0,0 +1,68 @@
+#' Raw Tidy Data for Three-Minute Squared Tasks
+#'
+#' Converts a messy e-prime data file into a tidy raw data file that is
+#' easy to work with.
+#'
+#' @param x dataframe
+#' @param include_col c(): list of additional columns to include
+#' @export
+#'
+
+raw_squared <- function(x, include_col = c()) {
+
+  x <- dplyr::rename(x, TrialProc = `Procedure[Trial]`)
+  if (!("FixationDuration" %in% colnames(x))) {
+    x <- dplyr::rename(x, FixationDuration = t4)
+  }
+  proc_names <- unique(x$TrialProc)
+  if ("pracproc" %in% proc_names) {
+    x <- dplyr::filter(x, TrialProc == "TrialProc" | TrialProc == "pracproc")
+    x <- dplyr::mutate(x,
+                       TrialProc =
+                         dplyr::case_when(TrialProc == "TrialProc" ~ "real",
+                                          TrialProc == "pracproc" ~ "practice"),
+                       Trial =
+                         dplyr::case_when(TrialProc == "real" ~
+                                            TrialList.Sample,
+                                          TrialProc == "practice" ~
+                                            practice.Sample))
+  } else {
+    x <- dplyr::select(x, -TrialProc)
+    x <- dplyr::rename(x, TrialProc = `Running[Trial]`)
+    x <- dplyr::filter(x,
+                       TrialProc == "TrialList" | TrialProc == "PracticeList2")
+    x <- dplyr::mutate(x,
+                       TrialProc =
+                         dplyr::case_when(TrialProc == "TrialList" ~
+                                            "real",
+                                          TrialProc == "PracticeList2" ~
+                                            "practice"),
+                       Trial =
+                         dplyr::case_when(TrialProc == "real" ~
+                                            TrialList.Sample,
+                                          TrialProc == "practice" ~
+                                            PracticeList2.Sample))
+  }
+
+  x <- dplyr::mutate(x,
+                     Target =
+                       dplyr::case_when(!is.na(right_targ) ~ right_targ,
+                                        !is.na(left_targ) ~ left_targ),
+                     TargetLocation = case_when(!is.na(right_cue) ~ "Left",
+                                                !is.na(left_cue) ~ "Right"))
+
+  if ("AdminTime" %in% colnames(x)) {
+    x <- dplyr::group_by(x, Subject)
+    x <- dplyr::mutate(x, AdminTime = dplyr::last(AdminTime) / 60000)
+    x <- dplyr::ungroup(x)
+    x <- dplyr::select(x, Subject, TrialProc, Trial, Accuracy = Mask.ACC,
+                       RT = Mask.RT, Target, FixationDuration,
+                       dplyr::all_of(include_col), AdminTime, SessionDate, SessionTime)
+  } else {
+    x <- dplyr::select(x, Subject, TrialProc, Trial, Accuracy = Mask.ACC,
+                       RT = Mask.RT, Target, FixationDuration,
+                       dplyr::all_of(include_col), SessionDate, SessionTime)
+  }
+
+  return(x)
+}
--- logo.R
+++ logo.R
@@ -2,14 +2,14 @@ library(hexSticker)
 library(here)
 
 sticker(here("man/figures",
-             "eagle.png"),
+             "awm_logo_small.jpeg"),
         filename = here("man/figures", "logo.png"),
         package = "englelab",
-        h_fill = "#004A85",
-        h_color = "#EAAA00",
-        p_color = "#FFFFFF",
-        p_size = 8,
-        s_width = .6,
+        h_fill = "#FFFFFF",
+        h_color = "#FFB002",
+        p_color = "#161D7A",
+        p_size = 16,
+        s_width = .45,
         s_x = 1.0,
-        s_y = .70)
+        s_y = .80)
 
